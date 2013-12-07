073d2e468a96b13315050217d8696239c5f09fe3
Cody Romano <codyromano@gmail.com>
2013-12-06 19:48:13 -0500

    Finish basic layout except for a few sections.

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype HTML>
+<html>
+<head>
+    <title> Map Streak - Go Streaking! </title>
+
+    <meta charset='utf-8'> 
+    <!-- For responsive design on mobile --> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Simple responsive grid --> 
+    <link rel="stylesheet" href="css/main.css" type="text/css"/> 
+
+</head>
+<body>
+
+<div class="page">
+
+    <header class="row">
+        <div class="header col">Map Streak Logo</div>
+    </header>
+
+    <div class="row">
+        <div class="span_2 col">Stick figure</div>
+        <div class="span_4 col">Tap Prompt</div>
+    </div>
+
+
+
+    <section id="tripIdeas"> 
+
+        <div class="row"> 
+            <h2 class="span_6 col"> Trip Ideas </h2>
+        </div>
+
+        <div class="row"> 
+            <div class="span_2 col"> Place image </div>
+            <div class="span_4 col"> 
+                <h3> Salvatore's Bistro </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at odio et nulla ultricies congue. 
+                Donec in leo sed justo sodales hendrerit eu in nisi.  <a href='#'> Read more </a> </p>
+            </div>
+        </div>
+
+        <div class="row"> 
+            <div class="span_2 col"> Place image </div>
+            <div class="span_4 col"> 
+                <h3> Chicago </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at odio et nulla ultricies congue. 
+                Donec in leo sed justo sodales hendrerit eu in nisi.  <a href='#'> Read more </a> </p>
+            </div>
+        </div>
+
+        <div class="row"> 
+            <div class="span_2 col"> Place image </div>
+            <div class="span_4 col"> 
+                <h3> New York </h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at odio et nulla ultricies congue. 
+                Donec in leo sed justo sodales hendrerit eu in nisi.  <a href='#'> Read more </a> </p>
+            </div>
+        </div>
+    </section>
+
+    <footer class="row">
+        <a href="#" class="span_2 col"> Credits </a>
+        <a href="#" class="span_2 col"> Backstory </a>
+        <a href="#" class="span_2 col"> Feedback </a>
+    </div>
+</div>
+
+</body>
+</html>
